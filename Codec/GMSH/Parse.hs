dd57951217a030422f5421d9f4f22e93dd284149
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-01 13:05:41 -0300

    feat(untested): adds a V1 mesh parser

--- Codec/GMSH/Parse.hs
+++ Codec/GMSH/Parse.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Codec.GMSH.Parse () where
+import qualified Codec.GMSH.Parse.V1 as V1 (mesh)
 import qualified Codec.GMSH.Parse.V2 as V2 (mesh)
 import Codec.GMSH.Types (Mesh)
 import Text.Parsec
 
 mesh :: Stream s m Char => ParsecT s u m Mesh
-mesh = V2.mesh
+mesh = choice [V1.mesh, V2.mesh]
---
+++ Codec/GMSH/Parse/V1.hs
@@ -0,0 +1,182 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Codec.GMSH.Parse.V1 (mesh) where
+
+import Control.Applicative hiding ((<|>))
+import Codec.GMSH.Parse.Commons(
+        coordinate,
+        float,
+        integer,
+        minus,
+        natural,
+        number,
+        plus
+        )
+import Codec.GMSH.Types(
+        Coordinate,
+        Element(..),
+        ElementType(..),
+        Format(..),
+        Mesh(..),
+        Node(..)
+        )
+import Text.Parsec
+
+-- Mesh Parse
+
+mesh :: Stream s m Char => ParsecT s u m Mesh
+mesh = do { ns  <- nodes
+          ; _   <- endOfLine
+          ; es  <- elements
+          ; return (Mesh (Format 1.0 0 8) ns es)
+          }
+
+-- Node Parse
+
+nodes :: Stream s m Char => ParsecT s u m [Node]
+nodes = do { _      <- string "$NOD"
+           ; _      <- endOfLine
+           ; ns     <- natural
+           ; _      <- endOfLine
+           ; nodes  <- count ns (node <* endOfLine)
+           ; _      <- string "$ENDNOD"
+           ; return nodes
+           }
+
+node :: Stream s m Char => ParsecT s u m Node
+node = do { index <- natural
+          ; _ <- spaces
+          ; x <- coordinate
+          ; _ <- spaces
+          ; y <- coordinate
+          ; _ <- spaces
+          ; z <- coordinate
+          ; return (Node index x y z)
+          }
+
+-- Element Parse
+
+elements :: Stream s m Char => ParsecT s u m [Element]
+elements =  do { _          <- string "$ELM"
+               ; _          <- endOfLine
+               ; es         <- natural
+               ; _          <- endOfLine
+               ; elements   <- count es (element <* endOfLine)
+               ; _          <- string "$ENDELM"
+               ; return elements
+               }
+
+element :: Stream s m Char => ParsecT s u m Element
+element = do { i        <- natural
+             ; _        <- spaces
+             ; (ts, es)
+                    <-  elementParse (string "1") line
+                    <|> elementParse (string "2") triangle
+                    <|> elementParse (string "3") quadrangle
+                    <|> elementParse (string "4") tetrahedron
+                    <|> elementParse (string "5") hexahedron
+                    <|> elementParse (string "8") line3
+                    <|> elementParse (string "9") triangle6
+             ; return (Element i ts es)
+             }
+
+elementParse :: Stream s m Char => (ParsecT s u m String) -> (ParsecT s u m ElementType) -> ParsecT s u m ([Int], ElementType)
+elementParse elmtype elmval = do { _    <- elmtype
+                                 ; _    <- spaces
+                                 ; tags <- elementTag
+                                 ; es   <- elmval
+                                 ; return (tags, es)
+                                 }
+
+-- Element Tags Parse
+
+elementTag :: Stream s m Char => ParsecT s u m [Int]
+elementTag = do { phys  <- natural
+                ; _     <- spaces
+                ; elem  <- natural
+                ; _     <- spaces
+                ; return [phys, elem]
+                }
+
+-- Element Type Parse
+
+hexahedron :: Stream s m Char => ParsecT s u m ElementType
+hexahedron = do { i0  <- natural
+                ; _   <- spaces
+                ; i1  <- natural
+                ; _   <- spaces
+                ; i2  <- natural
+                ; _   <- spaces
+                ; i3  <- natural
+                ; _   <- spaces
+                ; i4  <- natural
+                ; _   <- spaces
+                ; i5  <- natural
+                ; _   <- spaces
+                ; i6  <- natural
+                ; _   <- spaces
+                ; i7  <- natural
+                ; return (Hexahedron i0 i1 i2 i3 i4 i5 i6 i7)
+                }
+
+line :: Stream s m Char => ParsecT s u m ElementType
+line = do { i0  <- natural
+          ; _   <- spaces
+          ; i1  <- natural
+          ; return (Line i0 i1)
+          }
+
+line3 :: Stream s m Char => ParsecT s u m ElementType
+line3 = do { i0  <- natural
+           ; _   <- spaces
+           ; i1  <- natural
+           ; _   <- spaces
+           ; i2  <- natural
+           ; return (Line3 i0 i1 i2)
+           }
+
+quadrangle :: Stream s m Char => ParsecT s u m ElementType
+quadrangle = do { i0  <- natural
+                ; _   <- spaces
+                ; i1  <- natural
+                ; _   <- spaces
+                ; i2  <- natural
+                ; _   <- spaces
+                ; i3  <- natural
+                ; return (Quadrangle i0 i1 i2 i3)
+                }
+
+tetrahedron :: Stream s m Char => ParsecT s u m ElementType
+tetrahedron = do { i0  <- natural
+                 ; _   <- spaces
+                 ; i1  <- natural
+                 ; _   <- spaces
+                 ; i2  <- natural
+                 ; _   <- spaces
+                 ; i3  <- natural
+                 ; return (Tetrahedron i0 i1 i2 i3)
+                 }
+
+triangle :: Stream s m Char => ParsecT s u m ElementType
+triangle = do { i0  <- natural
+              ; _   <- spaces
+              ; i1  <- natural
+              ; _   <- spaces
+              ; i2  <- natural
+              ; return (Triangle i0 i1 i2)
+              }
+
+triangle6 :: Stream s m Char => ParsecT s u m ElementType
+triangle6 = do { i0  <- natural
+               ; _   <- spaces
+               ; i1  <- natural
+               ; _   <- spaces
+               ; i2  <- natural
+               ; _   <- spaces
+               ; i3  <- natural
+               ; _   <- spaces
+               ; i4  <- natural
+               ; _   <- spaces
+               ; i5  <- natural
+               ; return (Triangle6 i0 i1 i2 i3 i4 i5)
+               }
